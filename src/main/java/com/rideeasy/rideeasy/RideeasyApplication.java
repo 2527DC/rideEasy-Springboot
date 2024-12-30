dcf77efcf31b1f2159c34f173aa2cddeaf0d50f6
Chethan <chethan962089@gmail.com>
2024-12-30 16:53:56 +0530

    Initial commit

---
+++ src/main/java/com/rideeasy/rideeasy/RideeasyApplication.java
@@ -0,0 +1,13 @@
+package com.rideeasy.rideeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rideeasy/rideeasy/RideeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideeasy.rideeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
